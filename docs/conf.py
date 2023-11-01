06f2e5b825e21c48e46460abb524f8168f415a83
Ryan Tan <ryantanjunming@gmail.com>
2023-11-01 14:28:32 +0800

    typo

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,7 @@ todo_include_todos = False
 
 rst_prolog = """
 .. warning::
-   The Coralogix PHP SDK will End of Life(EOL) Feb 1st 2024.
-   For PHP PHP 7.4+, please use the `PHP OpenTelemetry Instrumentation <https://coralogix.com/docs/php-opentelemetry-instrumentation/>`_ instead.
+   | The Coralogix PHP SDK will End of Life(EOL) Feb 1st 2024.
+
+   | For PHP 7.4+, please use the `PHP OpenTelemetry Instrumentation <https://coralogix.com/docs/php-opentelemetry-instrumentation/>`_ instead.
 """
